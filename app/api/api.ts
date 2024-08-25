6a7eed14d8544295a9ab7d5be03cce60a447f133
Adetola Adedeji <adetolaadedeji0@gmail.com>
2024-08-25 07:53:39 +0100

    Update API cors

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 const BASE_URL =
-  "https://cors-anywhere.herokuapp.com/https://fa24-102-89-44-132.ngrok-free.app/finalProject1.qa_chain/run";
+  //   "https://cors-anywhere.herokuapp.com/https://fa24-102-89-44-132.ngrok-free.app/finalProject1.qa_chain/run";
 
-//"https://fa24-102-89-44-132.ngrok-free.app/finalProject1.qa_chain/run";
-// const BASE_URL = "https://cors-anywhere.herokuapp.com/http://localhost:8000/finalProject1.qa_chain/run";
+  "https://fa24-102-89-44-132.ngrok-free.app/finalProject1.qa_chain/run";
 
 export const Quilliri = async (userData: any) => {
   console.log("got here", userData);
   try {
     const response = await axios.post(`${BASE_URL}`, userData, {
-      // withCredentials: true, // Indicates whether or not cross-site Access-Control requests should be made using credentials
+      withCredentials: true, // Indicates whether or not cross-site Access-Control requests should be made using credentials
       // credentials: "include", // Passes cookies when making cross-origin requests
     });
     console.log(response);
